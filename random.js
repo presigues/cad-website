9c3dc8a0680a29ee79ee459aa6681e0ae1886a7b
pretzel <132411345+presigues@users.noreply.github.com>
2024-07-22 23:48:51 -0400

    final

--- random.js
+++
@@ -1,8 +0,0 @@
-AFRAME.registerComponent('button', {
-    init() {
-      const btn = document.getElementById('backtomain')
-      btn.addEventListener('click', () => {
-        window.open('https://google.com', '_blank')
-      })
-    }
-  })
